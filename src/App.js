9782905344f008438a95dc34c5279729f881eee6
Rezon Jay Tabigue <zontabigue@gmail.com>
2023-08-10 18:01:14 +0800

    update

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Layout />} >
+        <Route path='/' element={<Layout />} >
           <Route index element={<Home />} />
+          <Route path='/home' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
         </Route>
